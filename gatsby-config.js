54dcd9a1e25f730111d2a6692d3ce249a19d96e4
Pritesh <pritesh.hirani@cimpress.com>
2020-05-06 09:46:28 +0530

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,8 +85,8 @@ module.exports = {
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         // Defaults to null
-        defaultDataLayer: { platform: "gatsby" }
+        defaultDataLayer: { platform: "gatsby" },
       },
-    }
+    },
   ],
 }
